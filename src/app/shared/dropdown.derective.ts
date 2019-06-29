718b7bd506f02c80a3f358f1e8242e4d7cef8f8c
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-06-29 08:09:18 +0200

    added dropdown directive

---
+++ src/app/shared/dropdown.derective.ts
@@ -0,0 +1,19 @@
+import { Directive, HostListener, ElementRef, Input, HostBinding } from '@angular/core';
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective{
+    
+    @HostBinding('class.open') isOpen = false;
+    
+    // @HostListener('click') toggleOpen(eventData: Event){
+    //     this.isOpen = !this.isOpen;
+// }
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+    // console.log("event.target :" + event.target);
+    // console.log('this.elRef.nativeElement: ' + this.elRef.nativeElement)
+  }
+  constructor(private elRef: ElementRef) {}
+}
