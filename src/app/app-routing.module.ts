9aafdfb52500a7c0f6762fea25ea145913bc20d0
Krzysztof Skoneczny <ksdragon@poczta.pl>
2019-08-05 12:05:41 +0200

    add recipes-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,12 @@
-import { AuthGuardService } from './auth/auth.guard.service';
 import { AuthComponent } from './auth/auth.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
+
 
 const appRoutes: Routes = [
   // { path: '', component: RecipesComponent },
   { path: '', redirectTo: '/auth', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent,
-    canActivate: [AuthGuardService],            // dodajemy strażnika do ścieżki
-    children: [
-    { path: '' , component: RecipeStartComponent},
-    { path: 'new' , component: RecipeEditComponent},
-    { path: ':id' , component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-    { path: ':id/edit' , component: RecipeEditComponent,  resolve: [RecipesResolverService]}
-    ]},
   { path: 'shoppingList', component: ShoppingListComponent},
   { path: 'auth', component: AuthComponent }
 ];
---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,27 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { AuthGuardService } from '../auth/auth.guard.service';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipesResolverService } from './recipes-resolver.service';
+
+const routes: Routes = [
+  { path: 'recipes', component: RecipesComponent,
+    canActivate: [AuthGuardService],            // dodajemy strażnika do ścieżki
+    children: [
+    { path: '' , component: RecipeStartComponent},
+    { path: 'new' , component: RecipeEditComponent},
+    { path: ':id' , component: RecipeDetailComponent, resolve: [RecipesResolverService]},
+    { path: ':id/edit' , component: RecipeEditComponent,  resolve: [RecipesResolverService]}
+    ]}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class RecipesRoutingModule {
+
+}
